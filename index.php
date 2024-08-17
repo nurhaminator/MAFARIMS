e8b04ec416c6b0c2fbb2e8f625e224cefec633b0
nurhaminator <nurmustapha02@gmail.com>
2024-08-17 14:45:11 +0800

    removed the car img

--- index.php
+++ index.php
@@ -6,32 +6,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>MAFARIMS</title>
   <link rel="icon" href="assets/logo.png" type="image/x-icon" />
-  <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-    crossorigin="anonymous" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
   <link rel="stylesheet" href="index.css" />
 </head>
 
 <body>
   <section id="home">
-    <video
-      class="w-100 position-absolute"
-      loop
-      muted
-      autoplay
-      src="assets/bg-vid.mp4"></video>
+    <video class="w-100 position-absolute" loop muted autoplay src="assets/bg-vid.mp4"></video>
     <div class="container-fluid position-fixed">
       <nav class="navbar navbar-light">
         <div class="container-md">
           <a class="navbar-brand fs-3 logo text-success" href="#">
-            <img
-              src="assets/logo.png"
-              alt=""
-              width="60"
-              height="60"
-              class="d-inline-block align-text-center" />
+            <img src="assets/logo.png" alt="" width="60" height="60" class="d-inline-block align-text-center" />
             MAFARIMS
           </a>
           <div navbar-links>
@@ -51,20 +38,20 @@
         <div class="row">
           <div class="col-md-7" d-flex">
             <h1 class="tit text-success">MAFAR INVENTORY 🛠️<br />MANAGEMENT SYSTEM</h1>
-             <div class="col feat">
-                <h2>
-                  🛠️ Inventory Management <br>
-                  📦 Efficient Transfer Tracking <br>
-                  📈 Real-Time Confirmation <br>
-                  📊 Detailed Reporting</h2>
-              </div>
+            <div class="col feat">
+              <h2>
+                🛠️ Inventory Management <br>
+                📦 Efficient Transfer Tracking <br>
+                📈 Real-Time Confirmation <br>
+                📊 Detailed Reporting</h2>
             </div>
-            <div class="col-md-5">
-              <img src="assets/invenotry-grow.gif" class="wew" height="450px" alt="">
-            </div>
+          </div>
+          <div class="col-md-5">
+            <img src="assets/invenotry-grow.gif" class="wew" height="450px" alt="">
           </div>
         </div>
       </div>
+    </div>
     </div>
   </section>
 
@@ -97,12 +84,10 @@
     &copy; 2024 Developed by : CSU-BSIT Capstone Project
   </footer>
 
-  <script
-    src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
     integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
     crossorigin="anonymous"></script>
-  <script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
     integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
     crossorigin="anonymous"></script>
 </body>
